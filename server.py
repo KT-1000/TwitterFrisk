0c0c1bb2f1970bafbbd7a98281a40981d4ba05a7
annihilatrix <katie@katie.codes>
2016-06-27 00:54:57 -0700

    Starting templates.

--- server.py
+++ server.py
@@ -8,6 +8,13 @@ def index():
 
     return render_template("index.html")
 
+
+@app.route('/search')
+def search(user_str):
+    tweets = request.args.get()
+
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.debug = True
     app.run(use_reloader=True)
