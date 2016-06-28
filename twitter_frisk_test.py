dbb1817a9bd023a176bbcd8e32949a8216ffb01c
annihilatrix <katie@katie.codes>
2016-06-28 01:24:17 -0700

    Emergency save before bed

--- twitter_frisk_test.py
+++ twitter_frisk_test.py
@@ -1,20 +1,3 @@
-# Coding Challenge
-# Implement a twitter search engine that displays tweets containing a
-#     user submitted query string.
-
-# For each result tweet,
-    # display the name of the person who tweeted it,
-    # the content of the tweet,
-    # and the number of times it was favorited.
-
-# Separate from the list of tweets above,
-    # display a sidebar containing a list of hashtags present in the result set
-    # along with a count for number of times it was present for each hashtag.
-
-# The UI layout can be kept simple and is entirely up to you.
-
-# Please use Python/Flask along with the Twitter API for this exercise,
-# and stick to Python/Flask and code organization/design best practices as much as you can.
 import unittest
 import twitter_frisk as fts
 import secrets as sec
