71ec38f22c083942c99859c83a80cb9b2df691dd
annihilatrix <katie@katie.codes>
2016-06-27 20:40:57 -0700

    Getting data to populate in route

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, request, flash
+import twitter_frisk as fts
 
 app = Flask(__name__)
 
@@ -10,10 +11,15 @@ def index():
 
 
 @app.route('/search')
-def search(user_str):
-    tweets = request.args.get()
+def search():
+    status, tweets, hashtags = fts.frisk_tweets("cats")
+    # tweets = request.args.get()
+    hashtag_count = [11, 75, 99]
+    status_list = [99, 0, 241234234, 66]
 
-    return render_template("index.html")
+    return render_template("index.html",
+                           hashtag_count=hashtags,
+                           status_list=tweets)
 
 if __name__ == "__main__":
     app.debug = True
